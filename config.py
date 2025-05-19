27d1363d354de9c61cbf86a69a3867adbc231ac4
Jairos Nyirenda <jairos.pemphonyirenda@gmail.com>
2025-05-19 10:51:44 +0200

    removing unused configs

--- config.py
+++ config.py
@@ -4,15 +4,14 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-BACKUP_DIR = os.getenv("BACKUP_DIR", "")
-DOCS_DIR = os.getenv("DOCS_DIR", "")
+PGPASS_FILE = os.path.expanduser("~/.pgpass")
+
 DB_NAME = os.getenv("DB_NAME", "")
 DB_USER = os.getenv("DB_USER", "")
-DB_PASS = os.getenv("DB_PASS", "")
-S3_BUCKET = os.getenv("S3_BUCKET", "")
-AWS_REGION = os.getenv("AWS_REGION", "")
+
+BACKUP_DIR = os.getenv("BACKUP_DIR", "")
+DOCS_DIR = os.getenv("DOCS_DIR", "")
 LOGS_DIR = os.getenv("LOGS_DIR", "")
-PGPASS_FILE = os.path.expanduser("~/.pgpass")
 
 MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "")
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "")
